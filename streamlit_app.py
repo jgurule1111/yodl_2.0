8b6886b35c54b83fd1407bd0acf2dbabf27b9675
Jonny <131311280+jgurule1111@users.noreply.github.com>
2024-07-31 21:52:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title('🎈 App Name')
 
-st.write('Hello world!')
+user_input = st.text_area('Write something to activate the AI:', height=200)
+
+
+if st.button('Generate Story'):
+    with st.spinner('Generating Story...'):
+        response = graph.stream({"question": user_input}, config, stream_mode="values") #model(user_input, max_length=max_length, num_return_sequences=num_sequences)
+        for event in events:
+          _print_event(event, _printed)
