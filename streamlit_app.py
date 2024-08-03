30d46c9180bb0c713ba69c32f1bb746bfc82c4fc
Jonny <131311280+jgurule1111@users.noreply.github.com>
2024-08-02 21:02:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,16 @@
 import streamlit as st
 import os
 import pickle
+from codellama import test_poop
 
-st.title('🎈 Yodl')
-working_dir = os.path.dirname(os.path.abspath(__file__))
+st.title('Yodl')
 
-# loading the saved models
 
-model = pickle.load(open(f'{working_dir}/code/code1', 'r'))
+
 
 user_input = st.text_area('Write something to activate the AI:', height=200)
 
 
 if st.button('Generate Story'):
     with st.spinner('Generating Story...'):
-        response = graph.stream({"question": user_input}, config, stream_mode="values") #model(user_input, max_length=max_length, num_return_sequences=num_sequences)
-        for event in events:
-          _print_event(event, _printed)
+        response = test_poop(user_input)
