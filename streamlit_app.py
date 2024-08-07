ffd83023c0b2cbe38e707f2387e850fa5d85b6b0
Jonny <131311280+jgurule1111@users.noreply.github.com>
2024-08-06 21:21:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,16 @@
-import streamlit as st
-import os
-import codeforllama
+#import streamlit as st
+#import codeforllama
 
-st.title('Yodl')
-
-def test_poop(questions):
-    config = {
-    "configurable": {
-        # Checkpoints are accessed by thread_id
-        "thread_id": thread_id,
-     }
-    }
-    question = str(questions)
-    event = graph.invoke({"question": questions}, config)
-
-    message = event.get("messages")
-
-    if message:
-        if isinstance(message, list):
-            message = message[-1]
-            msg_repr = message.pretty_repr(html=True)
-
-    return print(msg_repr)
-
-    
+#st.title('Yodl')
 
 
 
     
 #user_input = st.text_area('Write something to activate the AI:', height=200)
-user_input = st.text_input('Enter your question:', placeholder = 'ex. what are the financial highlights for the year?')
+#user_input = st.text_input('Enter your question:', placeholder = 'ex. what are the financial highlights for the year?')
 
 
-if st.button('Enter'):
-    with st.spinner('Thinking...'):
-        response = test_poop(user_input)
-        st.write(response)
+#if st.button('Enter'):
+    #with st.spinner('Thinking...'):
+        #response = test_poop(user_input)
+        #st.write(response)
