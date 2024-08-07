f619c6eff5615f2c5e8552377454dfefd44e8799
Jonny <131311280+jgurule1111@users.noreply.github.com>
2024-08-06 20:08:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ st.title('Yodl')
 
 
 
-user_input = st.text_area('Write something to activate the AI:', height=200)
+#user_input = st.text_area('Write something to activate the AI:', height=200)
+user_input = st.text_input('Enter your question:', placeholder = 'ex. what are the financial highlights for the year?')
 
 
 if st.button('Enter'):
