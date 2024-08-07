8fff06a23aaa54214ab9be422471d5c22856a1b8
Jonny <131311280+jgurule1111@users.noreply.github.com>
2024-08-06 20:11:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title('Yodl')
 
 
 #user_input = st.text_area('Write something to activate the AI:', height=200)
-user_input = st.text_input('Enter your question:', placeholder = 'ex. what are the financial highlights for the year?')
+user_input = st.text_input('Enter your question:', placeholder = 'ex. what are the financial highlights for the year?, calculate the growth rate for Net Income for the year 2023?')
 
 
 if st.button('Enter'):
